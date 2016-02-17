af5162797b4714a08aafbc001671973e349d2f2f
yinkaisheng@foxmail.com <yinkaisheng@foxmail.com>
2016-02-17 10:42:20 +0800

    add get_system_devices.py

---
+++ get_system_devices.py
@@ -0,0 +1,21 @@
+#!python3
+# -*- coding: utf-8 -*-
+
+import time
+import subprocess
+import automation
+
+def main():
+    subprocess.Popen('mmc.exe devmgmt.msc')
+    mmcWindow = automation.WindowControl(ClassName = 'MMCMainFrame')
+    tree = automation.TreeControl(searchFromControl = mmcWindow)
+    for item, depth in automation.WalkTree(tree, getChildrenFunc = lambda c : c.GetChildren(), includeTop = True):
+        if isinstance(item, automation.TreeItemControl):  #or item.ControlType == automation.ControlType.TreeItemControl
+            item.Expand()
+            automation.Logger.WriteLine(' ' * (depth - 1) * 4 + item.Name, automation.ConsoleColor.Green)
+            time.sleep(0.1)
+
+if __name__ == '__main__':
+    main()
+    automation.GetConsoleWindow().SetActive()
+    input('\npress Enter to exit\n')
