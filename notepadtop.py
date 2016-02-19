2b2f1d6b626f7c5d83ece6e443ed19f5208b8b7e
yinkaisheng@foxmail.com <yinkaisheng@foxmail.com>
2016-02-19 18:00:30 +0800

    modified

--- notepadtop.py
+++ notepadtop.py
@@ -2,19 +2,21 @@
 # -*- coding:utf-8 -*-
 import sys
 import subprocess
-from automation import *
+import automation
 
 if __name__ == '__main__':
-    startTime = time.clock()
     isTop = 1
     print(sys.argv)
     if len(sys.argv) == 2:
         isTop = int(sys.argv[1])
-    note = WindowControl(searchDepth = 1, ClassName = 'Notepad')
+    note = automation.WindowControl(searchDepth = 1, ClassName = 'Notepad')
     if note.Exists(0, 0):
         note.SetTopmost(isTop)
     else:
         subprocess.Popen('notepad')
         note.Refind()
         note.SetTopmost(isTop)
-    print('done in {0:.3f}s'.format(time.clock()-startTime))
+        edit = automation.EditControl(searchFromControl= note)
+        edit.Click()
+        automation.SendKeys('I\'m a topmost window!!!')
+
