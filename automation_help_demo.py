7fb2b097c93963e90efd659051e8f0d7d6d5c787
yinkaisheng@foxmail.com <yinkaisheng@foxmail.com>
2016-11-08 13:53:47 +0800

    modify

--- automation_help_demo.py
+++ automation_help_demo.py
@@ -40,8 +40,8 @@ def DemoCN():
     automation.Win32API.MouseDragTo(l + 50, t + 10, 50, 10)
 
     thisWindow.SetActive()
-    automation.Logger.ColorfulWrite('我将运行<Color=Cyan>记事本</Color>并输入<Color=Cyan>你好!!!</Color>\n\n')
-    time.sleep(2)
+    automation.Logger.ColorfulWrite('我将运行<Color=Cyan>记事本</Color>并输入<Color=Cyan>Hello!!!</Color>\n\n')
+    time.sleep(3)
 
     subprocess.Popen('notepad')
     notepadWindow = automation.WindowControl(searchDepth = 1, ClassName = 'Notepad')
@@ -56,21 +56,21 @@ def DemoCN():
 
     thisWindow.SetActive()
     automation.Logger.ColorfulWrite('运行"<Color=Cyan>automation.py -h</Color>"显示帮助\n\n')
-    time.sleep(2)
+    time.sleep(3)
 
     cmdWindow.SendKeys('"{}" -h'.format(scriptPath) + '{Enter}', 0.05)
     time.sleep(3)
 
     thisWindow.SetActive()
     automation.Logger.ColorfulWrite('运行"<Color=Cyan>automation.py -r -d1</Color>"显示所有顶层窗口, 即桌面的子窗口\n\n')
-    time.sleep(2)
+    time.sleep(3)
 
     cmdWindow.SendKeys('"{}" -r -d1 -t0'.format(scriptPath) + '{Enter}', 0.05)
     time.sleep(3)
 
     thisWindow.SetActive()
-    automation.Logger.ColorfulWrite('运行"<Color=Cyan>automation.py -c</Color>"显示鼠标光标下的控件, 鼠标即将移动到记事本上\n\n')
-    time.sleep(2)
+    automation.Logger.ColorfulWrite('运行"<Color=Cyan>automation.py -c</Color>"显示鼠标光标下的控件\n\n')
+    time.sleep(3)
 
     cmdWindow.SendKeys('"{}" -c -t3'.format(scriptPath) + '{Enter}', 0.05)
     notepadWindow.SetActive()
@@ -80,7 +80,7 @@ def DemoCN():
 
     thisWindow.SetActive()
     automation.Logger.ColorfulWrite('运行"<Color=Cyan>automation.py -a</Color>"显示鼠标光标下的控件和它的所有父控件\n\n')
-    time.sleep(2)
+    time.sleep(3)
 
     cmdWindow.SendKeys('"{}" -a -t3'.format(scriptPath) + '{Enter}', 0.05)
     notepadWindow.SetActive()
@@ -90,7 +90,7 @@ def DemoCN():
 
     thisWindow.SetActive()
     automation.Logger.ColorfulWrite('运行"<Color=Cyan>automation.py</Color>"显示当前激活窗口和它的所有子控件\n\n')
-    time.sleep(2)
+    time.sleep(3)
 
     cmdWindow.SendKeys('"{}" -t3'.format(scriptPath) + '{Enter}', 0.05)
     notepadWindow.SetActive()
@@ -100,12 +100,86 @@ def DemoCN():
     time.sleep(3)
 
     thisWindow.SetActive()
-    automation.Logger.WriteLine('演示结束，按Enter退出', automation.ConsoleColor.Cyan)
+    automation.Logger.WriteLine('演示结束，按Enter退出', automation.ConsoleColor.Green)
     input()
 
 def DemoEN():
-    '''for English language'''
-    automation.Logger.WriteLine('not implement for your language', automation.ConsoleColor.Yellow)
+    '''for other language'''
+    thisWindow = automation.GetConsoleWindow()
+    automation.Logger.ColorfulWrite('I will run <Color=Cyan>cmd</Color>\n\n')
+    time.sleep(3)
+
+    automation.SendKeys('{Win}r')
+    while not isinstance(automation.GetFocusedControl(), automation.EditControl):
+        time.sleep(1)
+    automation.SendKeys('cmd{Enter}')
+    cmdWindow = automation.WindowControl(SubName = 'cmd.exe')
+    l, t, r, b = cmdWindow.BoundingRectangle
+    automation.Win32API.MouseDragTo(l + 50, t + 10, 50, 10)
+
+    thisWindow.SetActive()
+    automation.Logger.ColorfulWrite('I will run <Color=Cyan>Notepad</Color> and type <Color=Cyan>Hello!!!</Color>\n\n')
+    time.sleep(3)
+
+    subprocess.Popen('notepad')
+    notepadWindow = automation.WindowControl(searchDepth = 1, ClassName = 'Notepad')
+    cx, cy = automation.Win32API.GetScreenSize()
+    notepadWindow.MoveWindow(cx // 2, 0, cx // 2, cy // 2)
+    time.sleep(0.5)
+    notepadWindow.EditControl().SendKeys('Hello!!!', 0.05)
+    time.sleep(1)
+
+    dir = os.path.dirname(__file__)
+    scriptPath = os.path.join(dir, 'automation.py')
+
+    thisWindow.SetActive()
+    automation.Logger.ColorfulWrite('run "<Color=Cyan>automation.py -h</Color>" to display the help\n\n')
+    time.sleep(3)
+
+    cmdWindow.SendKeys('"{}" -h'.format(scriptPath) + '{Enter}', 0.05)
+    time.sleep(3)
+
+    thisWindow.SetActive()
+    automation.Logger.ColorfulWrite('run "<Color=Cyan>automation.py -r -d1</Color>" to display the top level windows, desktop\'s children\n\n')
+    time.sleep(3)
+
+    cmdWindow.SendKeys('"{}" -r -d1 -t0'.format(scriptPath) + '{Enter}', 0.05)
+    time.sleep(3)
+
+    thisWindow.SetActive()
+    automation.Logger.ColorfulWrite('run "<Color=Cyan>automation.py -c</Color>" to display the control under mouse cursor\n\n')
+    time.sleep(3)
+
+    cmdWindow.SendKeys('"{}" -c -t3'.format(scriptPath) + '{Enter}', 0.05)
+    notepadWindow.SetActive()
+    notepadWindow.MoveCursorToMyCenter()
+    time.sleep(3)
+    cmdWindow.SetActive(waitTime = 2)
+
+    thisWindow.SetActive()
+    automation.Logger.ColorfulWrite('run "<Color=Cyan>automation.py -a</Color>" to display the control under mouse cursor and its ancestors\n\n')
+    time.sleep(3)
+
+    cmdWindow.SendKeys('"{}" -a -t3'.format(scriptPath) + '{Enter}', 0.05)
+    notepadWindow.SetActive()
+    notepadWindow.MoveCursorToMyCenter()
+    time.sleep(3)
+    cmdWindow.SetActive(waitTime = 2)
+
+    thisWindow.SetActive()
+    automation.Logger.ColorfulWrite('run "<Color=Cyan>automation.py</Color>" to display the active window\n\n')
+    time.sleep(3)
+
+    cmdWindow.SendKeys('"{}" -t3'.format(scriptPath) + '{Enter}', 0.05)
+    notepadWindow.SetActive()
+    notepadWindow.EditControl().Click()
+    time.sleep(3)
+    cmdWindow.SetActive(waitTime = 2)
+    time.sleep(3)
+
+    thisWindow.SetActive()
+    automation.Logger.WriteLine('press Enter to exit', automation.ConsoleColor.Green)
+    input()
 
 if __name__ == '__main__':
     uiLanguage = ctypes.windll.kernel32.GetUserDefaultUILanguage()
