4ffcaf649754f2693a074c62c4de1643ed9a0ac5
yinkaisheng <yinkaisheng@foxmail.com>
2017-09-18 21:40:20 +0800

    modify demo

--- demos/get_system_devices.py
+++ demos/get_system_devices.py
@@ -11,6 +11,7 @@ import uiautomation as automation
 
 
 def main():
+    cmdWindow = automation.GetConsoleWindow()
     subprocess.Popen('mmc.exe devmgmt.msc')
     time.sleep(1)
     mmcWindow = automation.WindowControl(searchDepth = 1, ClassName = 'MMCMainFrame')
@@ -22,8 +23,57 @@ def main():
                 item.Expand(0)
             automation.Logger.WriteLine(' ' * (depth - 1) * 4 + item.Name, automation.ConsoleColor.Green)
             time.sleep(0.1)
+    if cmdWindow:
+        automation.Logger.ColorfulWriteLine('Scroll to top by SendKeys <Color=Cyan>Ctrl+Home</Color>')
+        cmdWindow.SetActive(waitTime = 1)
+    mmcWindow.SetActive(waitTime = 1)
+    tree.SendKeys('{Ctrl}{Home}', waitTime = 1)
+    if cmdWindow:
+        automation.Logger.ColorfulWriteLine('Scroll to bottom by SendKeys <Color=Cyan>Ctrl+End</Color>')
+        cmdWindow.SetActive(waitTime = 1)
+    mmcWindow.SetActive(waitTime = 1)
+    tree.SendKeys('{Ctrl}{End}', waitTime = 1)
+    if cmdWindow:
+        automation.Logger.ColorfulWriteLine('Scroll to top by <Color=Cyan>WheelUp</Color>')
+        cmdWindow.SetActive(waitTime = 1)
+    #vScrollBar = tree.ScrollBarControl(AutomationId = 'NonClientVerticalScrollBar')# failed to get scollbar sometimes, why?
+    vScrollBar = mmcWindow.ScrollBarControl(AutomationId = 'NonClientVerticalScrollBar')
+    vScrollBarRect = vScrollBar.BoundingRectangle
+    thumb = vScrollBar.ThumbControl()
+    while True:
+        vPercent = tree.CurrentVerticalScrollPercent()
+        vPercent2 = vScrollBar.RangeValuePatternCurrentValue()
+        print('TreeControl.CurrentVerticalScrollPercent', vPercent)
+        print('ScrollBarControl.RangeValuePatternCurrentValue', vPercent)
+        if vPercent2 > 0:
+            tree.WheelUp(waitTime = 0.1)
+        else:
+            break
+    if cmdWindow:
+        automation.Logger.ColorfulWriteLine('Scroll to bottom by <Color=Cyan>WheelDown</Color>')
+        cmdWindow.SetActive(waitTime = 1)
+    while True:
+        vPercent = tree.CurrentVerticalScrollPercent()
+        vPercent2 = vScrollBar.RangeValuePatternCurrentValue()
+        print('TreeControl.CurrentVerticalScrollPercent', vPercent)
+        print('ScrollBarControl.RangeValuePatternCurrentValue', vPercent)
+        if vPercent2 < 100:
+            tree.WheelDown(waitTime = 0.1)
+        else:
+            break
+    if cmdWindow:
+        automation.Logger.ColorfulWriteLine('Scroll by <Color=Cyan>DragDrop</Color>')
+        cmdWindow.SetActive(waitTime = 1)
+    mmcWindow.SetActive(waitTime = 1)
+    x, y = thumb.MoveCursorToMyCenter()
+    automation.DragDrop(x, y, x, vScrollBarRect[1])
+    x, y = thumb.MoveCursorToMyCenter()
+    automation.DragDrop(x, y, x, vScrollBarRect[3])
+
 
 if __name__ == '__main__':
     main()
-    automation.GetConsoleWindow().SetActive()
+    cmdWindow = automation.GetConsoleWindow()
+    if cmdWindow:
+        cmdWindow.SetActive()
     input('\npress Enter to exit\n')
