093c420da7d4d6111ae744328da620d24096aea4
yinkaisheng <yinkaisheng@foxmail.com>
2019-03-14 09:50:59 +0800

    modify RunWithHotKey, scroll demo

--- demos/automation_devicemanager.py
+++ demos/automation_devicemanager.py
@@ -7,34 +7,49 @@ import time
 import subprocess
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # not required after 'pip install uiautomation'
-import uiautomation as automation
+import uiautomation as auto
 
 
 def main():
-    cmdWindow = automation.GetConsoleWindow()
+    sw, sh = auto.Win32API.GetScreenSize()
+    cmdWindow = auto.GetConsoleWindow()
     subprocess.Popen('mmc.exe devmgmt.msc')
     time.sleep(1)
-    mmcWindow = automation.WindowControl(searchDepth = 1, ClassName = 'MMCMainFrame')
+    mmcWindow = auto.WindowControl(searchDepth = 1, ClassName = 'MMCMainFrame')
+    mmcWindow.Move(0, 0)
+    mmcWindow.Resize(sw // 2, sh * 3 // 4)
+    cmdWindow.Move(sw // 2, 0)
+    cmdWindow.Resize(sw // 2, sh * 3 // 4)
     tree = mmcWindow.TreeControl()
-    for item, depth in automation.WalkControl(tree, includeTop = True):
-        if isinstance(item, automation.TreeItemControl):  #or item.ControlType == automation.ControlType.TreeItemControl
+    for item, depth in auto.WalkControl(tree, includeTop=True):
+        if isinstance(item, auto.TreeItemControl):  #or item.ControlType == auto.ControlType.TreeItemControl
             item.Select()
-            if automation.ExpandCollapseState.Collapsed == item.CurrentExpandCollapseState():
+            if auto.ExpandCollapseState.Collapsed == item.CurrentExpandCollapseState():
                 item.Expand(0)
-            automation.Logger.WriteLine(' ' * (depth - 1) * 4 + item.Name, automation.ConsoleColor.Green)
-            time.sleep(0.1)
+            auto.Logger.WriteLine(' ' * (depth - 1) * 4 + item.Name, auto.ConsoleColor.Green)
+            time.sleep(0.05)
     if cmdWindow:
-        automation.Logger.ColorfulWriteLine('Scroll to top by SendKeys <Color=Cyan>Ctrl+Home</Color>')
+        auto.Logger.ColorfulWriteLine('Scroll by <Color=Cyan>SetScrollPercent</Color>')
+        cmdWindow.SetActive(waitTime=1)
+    mmcWindow.SetActive(waitTime=1)
+    for v in range(100, -1, -10):
+        tree.SetScrollPercent(0, v)
+        time.sleep(0.05)
+    for v in range(0, 101, 10):
+        tree.SetScrollPercent(0, v)
+        time.sleep(0.05)
+    if cmdWindow:
+        auto.Logger.ColorfulWriteLine('Scroll to top by SendKeys <Color=Cyan>Ctrl+Home</Color>')
         cmdWindow.SetActive(waitTime = 1)
     mmcWindow.SetActive(waitTime = 1)
     tree.SendKeys('{Ctrl}{Home}', waitTime = 1)
     if cmdWindow:
-        automation.Logger.ColorfulWriteLine('Scroll to bottom by SendKeys <Color=Cyan>Ctrl+End</Color>')
+        auto.Logger.ColorfulWriteLine('Scroll to bottom by SendKeys <Color=Cyan>Ctrl+End</Color>')
         cmdWindow.SetActive(waitTime = 1)
     mmcWindow.SetActive(waitTime = 1)
     tree.SendKeys('{Ctrl}{End}', waitTime = 1)
     if cmdWindow:
-        automation.Logger.ColorfulWriteLine('Scroll to top by <Color=Cyan>WheelUp</Color>')
+        auto.Logger.ColorfulWriteLine('Scroll to top by <Color=Cyan>WheelUp</Color>')
         cmdWindow.SetActive(waitTime = 1)
     print(tree.Handle, tree.Element, len(tree.GetChildren()))
     # before expand, tree has no scrollbar. after expand, tree has a scrollbar.
@@ -51,11 +66,11 @@ def main():
         print('TreeControl.CurrentVerticalScrollPercent', vPercent)
         print('ScrollBarControl.RangeValuePatternCurrentValue', vPercent2)
         if vPercent2 > 0:
-            tree.WheelUp(waitTime = 0.1)
+            tree.WheelUp(waitTime=0.05)
         else:
             break
     if cmdWindow:
-        automation.Logger.ColorfulWriteLine('Scroll to bottom by <Color=Cyan>WheelDown</Color>')
+        auto.Logger.ColorfulWriteLine('Scroll to bottom by <Color=Cyan>WheelDown</Color>')
         cmdWindow.SetActive(waitTime = 1)
     while True:
         vPercent = tree.CurrentVerticalScrollPercent()
@@ -63,23 +78,23 @@ def main():
         print('TreeControl.CurrentVerticalScrollPercent', vPercent)
         print('ScrollBarControl.RangeValuePatternCurrentValue', vPercent2)
         if vPercent2 < 100:
-            tree.WheelDown(waitTime = 0.1)
+            tree.WheelDown(waitTime = 0.05)
         else:
             break
     if cmdWindow:
-        automation.Logger.ColorfulWriteLine('Scroll by <Color=Cyan>DragDrop</Color>')
-        cmdWindow.SetActive(waitTime = 1)
+        auto.Logger.ColorfulWriteLine('Scroll by <Color=Cyan>DragDrop</Color>')
+        cmdWindow.SetActive(waitTime=1)
     mmcWindow.SetActive(waitTime = 1)
     x, y = thumb.MoveCursorToMyCenter()
-    automation.DragDrop(x, y, x, vScrollBarRect[1], waitTime = 1)
+    auto.DragDrop(x, y, x, vScrollBarRect[1], waitTime = 1)
     x, y = thumb.MoveCursorToMyCenter()
-    automation.DragDrop(x, y, x, vScrollBarRect[3])
+    auto.DragDrop(x, y, x, vScrollBarRect[3])
     mmcWindow.Close()
 
 
 if __name__ == '__main__':
     main()
-    cmdWindow = automation.GetConsoleWindow()
+    cmdWindow = auto.GetConsoleWindow()
     if cmdWindow:
         cmdWindow.SetActive()
-    input('\npress Enter to exit\n')
+    auto.Logger.WriteLine('\npress Enter to exit\n')
