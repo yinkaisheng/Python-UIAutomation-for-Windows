4964cebe7f1a6ebf6be0785729128a1d95ce9f92
yinkaisheng <yinkaisheng@foxmail.com>
2019-03-23 17:47:05 +0800

    update for python uiautomation 2.0.0

--- demos/hide_window_with_hotkey.py
+++ demos/hide_window_with_hotkey.py
@@ -7,6 +7,7 @@ import sys
 import time
 import ctypes
 import subprocess
+import psutil
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # not required after 'pip install uiautomation'
 import uiautomation as auto
--- demos/hotkey_test.py
+++ demos/hotkey_test.py
@@ -10,17 +10,30 @@ import uiautomation as auto
 
 
 def test1(stopEvent):
+    """
+    This function runs in a new thread triggered by hotkey.
+    """
+    auto.InitializeUIAutomationInThisThread()
     n = 0
+    child = None
+    auto.Logger.WriteLine('Use UIAutomation in another thread:', auto.ConsoleColor.Yellow)
     while True:
         if stopEvent.is_set():
             break
-        print(n)
+        if not child:
+            n = 1
+            child = auto.GetRootControl().GetFirstChildControl()
+        auto.Logger.WriteLine(n, auto.ConsoleColor.Cyan)
+        auto.LogControl(child)
+        child = child.GetNextSiblingControl()
         n += 1
         stopEvent.wait(1)
+    auto.UninitializeUIAutomationInThisThread()
     print('test1 exits')
 
 
 def test2(stopEvent):
+    """This function runs in a thread triggered by hotkey"""
     p = subprocess.Popen('python.exe automation_notepad.py')
     auto.Logger.WriteLine('call python.exe automation_notepad.py', auto.ConsoleColor.DarkGreen)
     while True:
@@ -43,6 +56,9 @@ def main():
         oriTitle = auto.GetConsoleTitle()
     auto.SetConsoleTitle(auto.GetConsoleTitle() + ' | Ctrl+1,Ctrl+2, stop Ctrl+4')
     auto.Logger.ColorfullyWriteLine('Press <Color=Cyan>Ctrl+1 or Ctrl+2</Color> to start, press <Color=Cyan>ctrl+4</Color> to stop')
+    auto.Logger.Write('\nUse UIAutomation in main thread:\n', auto.ConsoleColor.Yellow)
+    auto.LogControl(auto.GetRootControl())
+    auto.Logger.Write('\n')
     auto.RunByHotKey({(auto.ModifierKey.Control, auto.Keys.VK_1): test1,
                               (auto.ModifierKey.Control, auto.Keys.VK_2): test2},
                              (auto.ModifierKey.Control, auto.Keys.VK_4))
