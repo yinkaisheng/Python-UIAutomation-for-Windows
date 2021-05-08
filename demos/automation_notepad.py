4c6a95f5f1dfc4962fe59bdd2a29dda7254f2ecc
yinkaisheng <yinkaisheng@agora.io>
2021-05-08 11:55:03 +0800

    modify notepad demo

--- demos/automation_notepad.py
+++ demos/automation_notepad.py
@@ -1,8 +1,5 @@
 #!python3
 # -*- coding: utf-8 -*-
-# If your monitor is not 100% DPI and OS is Windows 10 x64, please run this script with 64 bit Python.
-# Windows 10 has a bug that 32 bit progroms can't get the correct BoundingRectangle of some controls of Notepad.
-# See https://github.com/microsoft/accessibility-insights-windows/issues/1122
 import os
 import sys
 import time
@@ -25,19 +22,27 @@ This means that the code can be freely copied and distributed, and costs nothing
 代码原理介绍: http://www.cnblogs.com/Yinkaisheng/p/3444132.html
 """
 
+tip = """
+If your monitor is not 100% DPI and OS is Windows 10 x64, please run this script with 64 bit Python.
+Windows 10 has a bug that 32 bit progroms can't get the correct BoundingRectangle of some controls of Notepad.
+See https://github.com/microsoft/accessibility-insights-windows/issues/1122
+"""
+
 
 def testNotepadCN():
     consoleWindow = auto.GetConsoleWindow()
     consoleWindow.SetActive()
+    auto.Logger.WriteLine(tip, auto.ConsoleColor.Yellow)
+    time.sleep(2)
     auto.Logger.ColorfullyWriteLine('\nI will open <Color=Green>Notepad</Color> and <Color=Yellow>automate</Color> it. Please wait for a while.')
     time.sleep(2)
     auto.ShowDesktop()
-    #打开notepad
+    # 打开notepad
     subprocess.Popen('notepad')
-    #查找notepad， 如果name有中文，python2中要使用Unicode
-    window = auto.WindowControl(searchDepth = 1, ClassName = 'Notepad', RegexName = '.* - 记事本')
-    #可以判断window是否存在，如果不判断，找不到window的话会抛出异常
-    #if window.Exists(maxSearchSeconds = 3):
+    # 查找notepad
+    window = auto.WindowControl(searchDepth=1, ClassName='Notepad', RegexName='.* - 记事本')
+    # 可以判断window是否存在，如果不判断，找不到window的话会抛出异常
+    # if window.Exists(maxSearchSeconds = 3):
     if auto.WaitForExist(window, 3):
         auto.Logger.WriteLine("Notepad exists now")
     else:
@@ -45,24 +50,23 @@ def testNotepadCN():
     screenWidth, screenHeight = auto.GetScreenSize()
     window.MoveWindow(screenWidth // 4, screenHeight // 4, screenWidth // 2, screenHeight // 2)
     window.SetActive()
-    #查找edit
-    edit = auto.EditControl(searchFromControl = window)  #or edit = window.EditControl()
-    edit.Click(waitTime = 0)
-    #python2中要使用Unicode, 模拟按键
+    # 查找edit
+    edit = auto.EditControl(searchFromControl=window)  # or edit = window.EditControl()
+    edit.Click(waitTime=0)
     edit.GetValuePattern().SetValue('hi你好')
     edit.SendKeys('{Ctrl}{End}{Enter}下面开始演示{! 4}{ENTER}', 0.2, 0)
     edit.SendKeys(text)
-    edit.SendKeys('{Enter 3}0123456789{Enter}', waitTime = 0)
-    edit.SendKeys('ABCDEFGHIJKLMNOPQRSTUVWXYZ{ENTER}', waitTime = 0)
-    edit.SendKeys('abcdefghijklmnopqrstuvwxyz{ENTER}', waitTime = 0)
-    edit.SendKeys('`~!@#$%^&*()-_=+{ENTER}', waitTime = 0)
-    edit.SendKeys('[]{{}{}}\\|;:\'\",<.>/?{ENTER}', waitTime = 0)
+    edit.SendKeys('{Enter 3}0123456789{Enter}', waitTime=0)
+    edit.SendKeys('ABCDEFGHIJKLMNOPQRSTUVWXYZ{ENTER}', waitTime=0)
+    edit.SendKeys('abcdefghijklmnopqrstuvwxyz{ENTER}', waitTime=0)
+    edit.SendKeys('`~!@#$%^&*()-_=+{ENTER}', waitTime=0)
+    edit.SendKeys('[]{{}{}}\\|;:\'\",<.>/?{ENTER}', waitTime=0)
     edit.SendKeys('™®①②③④⑤⑥⑦⑧⑨⑩§№☆★○●◎◇◆□℃‰€■△▲※→←↑↓〓¤°＃＆＠＼＾＿―♂♀{ENTER}{CTRL}a')
     window.CaptureToImage('Notepad.png')
     edit.SendKeys('Image Notepad.png was captured, you will see it later.', 0.05)
-    #查找菜单
-    window.MenuItemControl(Name = '格式(O)').Click()
-    window.MenuItemControl(Name = '字体(F)...').Click()
+    # 查找菜单
+    window.MenuItemControl(Name='格式(O)').Click()
+    window.MenuItemControl(Name='字体(F)...').Click()
     windowFont = window.WindowControl(Name='字体')
     listItem = windowFont.ListControl(searchDepth=2, AutomationId='1000').ListItemControl(Name='微软雅黑')
     if listItem.Exists(2):
@@ -95,7 +99,7 @@ def testNotepadCN():
     # 使用另一种查找方法
     buttonNotSave = window.ButtonControl(Compare=lambda control, depth: '不保存' in control.Name or '否' in control.Name)
     buttonNotSave.Click()
-    subprocess.Popen('Notepad.png', shell = True)
+    subprocess.Popen('Notepad.png', shell=True)
     time.sleep(2)
     consoleWindow.SetActive()
     auto.Logger.WriteLine('script exits', auto.ConsoleColor.Cyan)
@@ -109,10 +113,10 @@ def testNotepadEN():
     time.sleep(2)
     auto.ShowDesktop()
     subprocess.Popen('notepad')
-    #search notepad window, if searchFromControl is None, search from RootControl
-    #searchDepth = 1 makes searching faster, only searches RootControl's children, not children's children
-    window = auto.WindowControl(searchDepth = 1, ClassName = 'Notepad', RegexName = '.* - Notepad')
-    #if window.Exists(maxSearchSeconds = 3): #check before using it
+    # search notepad window, if searchFromControl is None, search from RootControl
+    # searchDepth = 1 makes searching faster, only searches RootControl's children, not children's children
+    window = auto.WindowControl(searchDepth=1, ClassName='Notepad', RegexName='.* - Notepad')
+    # if window.Exists(maxSearchSeconds = 3): #check before using it
     if auto.WaitForExist(window, 3):
         auto.Logger.WriteLine("Notepad exists now")
     else:
@@ -120,22 +124,22 @@ def testNotepadEN():
     screenWidth, screenHeight = auto.GetScreenSize()
     window.MoveWindow(screenWidth // 4, screenHeight // 4, screenWidth // 2, screenHeight // 2)
     window.SetActive()
-    edit = auto.EditControl(searchFromControl = window)  #or edit = window.EditControl()
-    edit.Click(waitTime = 0)
+    edit = auto.EditControl(searchFromControl=window)  # or edit = window.EditControl()
+    edit.Click(waitTime=0)
     edit.GetValuePattern().SetValue('hi你好')
     edit.SendKeys('{Ctrl}{End}{Enter}下面开始演示{! 4}{ENTER}', 0.2, 0)
     edit.SendKeys(text)
-    edit.SendKeys('{Enter 3}0123456789{Enter}', waitTime = 0)
-    edit.SendKeys('ABCDEFGHIJKLMNOPQRSTUVWXYZ{Enter}', waitTime = 0)
-    edit.SendKeys('abcdefghijklmnopqrstuvwxyz{Enter}', waitTime = 0)
-    edit.SendKeys('`~!@#$%^&*()-_=+{Enter}', waitTime = 0)
+    edit.SendKeys('{Enter 3}0123456789{Enter}', waitTime=0)
+    edit.SendKeys('ABCDEFGHIJKLMNOPQRSTUVWXYZ{Enter}', waitTime=0)
+    edit.SendKeys('abcdefghijklmnopqrstuvwxyz{Enter}', waitTime=0)
+    edit.SendKeys('`~!@#$%^&*()-_=+{Enter}', waitTime=0)
     edit.SendKeys('[]{{}{}}\\|;:\'\",<.>/?{Enter}{Ctrl}a')
     window.CaptureToImage('Notepad.png')
     edit.SendKeys('Image Notepad.png was captured, you will see it later.', 0.05)
-    #find menu
-    window.MenuItemControl(Name = 'Format').Click()
-    window.MenuItemControl(Name = 'Font...').Click()
-    windowFont = window.WindowControl(Name = 'Font')
+    # find menu
+    window.MenuItemControl(Name='Format').Click()
+    window.MenuItemControl(Name='Font...').Click()
+    windowFont = window.WindowControl(Name='Font')
 
     comboIndex = 0
     selectIndex = 2
@@ -150,7 +154,7 @@ def testNotepadEN():
     combo.Select(condition=select3rd)
     combo.Select('Western')
 
-    windowFont.ButtonControl(Name = 'OK').Click()
+    windowFont.ButtonControl(Name='OK').Click()
     window.Close()
     if auto.WaitForDisappear(window, 3):
         auto.Logger.WriteLine("Notepad closed")
@@ -163,11 +167,12 @@ def testNotepadEN():
     # another way to find the button using lambda
     buttonNotSave = window.ButtonControl(Compare=lambda control, depth: 'Don\'t Save' in control.Name or 'No' in control.Name)
     buttonNotSave.Click()
-    subprocess.Popen('Notepad.png', shell = True)
+    subprocess.Popen('Notepad.png', shell=True)
     time.sleep(2)
     consoleWindow.SetActive()
     auto.Logger.WriteLine('script exits', auto.ConsoleColor.Cyan)
     time.sleep(2)
+
 
 if __name__ == '__main__':
     uiLanguage = ctypes.windll.kernel32.GetUserDefaultUILanguage()
