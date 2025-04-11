39d7d65635d29e7aafea51c637ee85cf3335dfef
yinkaisheng <yinkaisheng@foxmail.com>
2025-04-11 17:24:51 +0800

    add setup.py sdist

--- upload_pypi.bat
+++ upload_pypi.bat
@@ -1,6 +1,6 @@
 del /q dist\*
 SET python=D:\Python38x64\python.exe
-%python% setup.py bdist_wheel
+%python% setup.py sdist bdist_wheel
 %python% rename_wheel.py
 pause
 %python% -m twine upload dist\*
